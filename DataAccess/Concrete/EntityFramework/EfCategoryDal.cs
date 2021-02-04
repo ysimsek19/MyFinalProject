bc54dcdf6fa861e256a20a024f4960637b74bcda
yalcinsimsek <ysimsek@karatekin.edu.tr>
2021-02-04 16:47:43 +0300

    04.02.2020

---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class EfCategoryDal : ICategoryDal
+	{
+		public void Add(Catagory entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Catagory entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Catagory Get(Expression<Func<Catagory, bool>> filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Catagory> GetAll(Expression<Func<Catagory, bool>> filter = null)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Catagory entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
